536f853f744b3bd9b17e55a14be5e679eb193cce
github username <github email address>
2017-05-12 10:24:32 -0400

    completed_basic_requirements

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -2,6 +2,7 @@ class AppearancesController < ApplicationController
 
   def new
     @appearance = Appearance.new
+    @user = current_user
   end
 
   def create
@@ -13,10 +14,26 @@ class AppearancesController < ApplicationController
     end
   end
 
+      def edit
+          @appearance = Appearance.find(params[:id])
+      end
+
+      def update
+          @appearance = Appearance.find(params[:id])
+          @appearance.update(appearance_params)
+
+          redirect_to episode_path(@appearance.episode)
+
+      end
+
+      def index
+          @appearances = Appearance.all
+      end
+
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating, :user_id)
   end
 
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,5 +1,9 @@
 class Appearance < ApplicationRecord
+  validates :rating, numericality: { less_than_or_equal_to: 5,  only_integer: true }
+
   belongs_to :guest
   belongs_to :episode
-  
+  belongs_to :user
+
+
 end
